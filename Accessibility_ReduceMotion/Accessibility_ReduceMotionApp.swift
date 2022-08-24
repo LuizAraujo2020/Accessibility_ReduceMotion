950ea042341ceaf69c116677e1fdb8d0f7ad9d6e
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-08-24 20:27:21 -0300

    Initial Commit

---
+++ Accessibility_ReduceMotion/Accessibility_ReduceMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_ReduceMotionApp.swift
+//  Accessibility_ReduceMotion
+//
+//  Created by Luiz Araujo on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_ReduceMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility_ReduceMotion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility_ReduceMotion
+//
+//  Created by Luiz Araujo on 24/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
